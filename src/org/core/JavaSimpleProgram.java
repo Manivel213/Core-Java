be9eb9d213b3aabfcecba8101fec3a004cd8fdaf
manivel213 <maniveltestengineer213@gmail.com>
2023-03-08 15:24:05 +0530

    first commit

---
+++ src/org/core/JavaSimpleProgram.java
@@ -0,0 +1,16 @@
+package org.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaSimpleProgram {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\eclipse-workspace\\CoreJavaSample\\Drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+	}
+
+}
