447da921436eab3448a8a3c94b2147d2fa243e24
Manivel213 <125730487+Manivel213@users.noreply.github.com>
2023-04-19 17:46:50 +0530

    Array Method Coding Sample

---
+++ Array Class/SampleProgram.java
@@ -0,0 +1,27 @@
+package org.array;
+
+public class SampleProgram {
+		
+		public static void main(String[] args) {
+			
+			int tamil1[] = new int[5];
+			tamil1[0] = 90;
+			tamil1[3] = 56;
+			tamil1[4] = 89;
+			tamil1[2] = 97;
+			tamil1[1] = 99;
+			
+			for (int i = 0; i < tamil1.length; i++) {
+				System.out.println(tamil1[i]);
+				
+				
+				
+				
+				
+			}
+			
+		}
+
+	}
+		
+
