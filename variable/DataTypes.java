2157214d99cc61098c67bcb4efbf0ff93cc60cdd
Manivel213 <125730487+Manivel213@users.noreply.github.com>
2023-04-19 17:08:43 +0530

    Add files via upload

---
+++ variable/DataTypes.java
@@ -0,0 +1,13 @@
+package org.variable;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		int ticketPrice = 900;
+		boolean paid = true;
+		char c = 't';
+		System.out.println("ticketPrice is " + ticketPrice);
+		System.out.println(paid);
+		System.out.println(ticketPrice);
+	}
+
+}
